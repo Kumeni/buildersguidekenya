fd0a7b85c75546003ce7122b794be7d35aa22bb8
Samson Kumeni <samsonkumeni8@gmail.com>
2020-09-03 20:09:03 +0300

    second commit

--- components/header/header.js
+++ components/header/header.js
@@ -7,7 +7,10 @@ function Header(){
                     <div className={style.div + ' ' +style.bgkLogo} tabIndex='1'>
                         <img className={style.bgkIcon} src='/icons/builders guide logo-01.png' alt='buildersguidekenyalogo'/>
                     </div>
-                    <input className={style.searchBar + ' ' + style.div} type='search' tabIndex='3' />
+                    <div className={style.div}>
+                        <img className={style.searchIcon} src='/icons/icons8-search-50.png' alt='icons8-search-50.png' />
+                        <input className={style.searchBar} type='search' tabIndex='3' />
+                    </div>
                     <div className={style.myAccountContainer} tabIndex='5'>
                         <img className={style.icon} src="/icons/icons8-person-64.png" alt="icons8-search-50.png"/><span className={style.myAccount}>My Account</span>
                     </div>
--- components/trendingComponent/trendingComponent.js
+++ components/trendingComponent/trendingComponent.js
@@ -1,5 +1,6 @@
 import style from './TrendingComponent.module.css'
 import SectionTitle from '../categorySections/sectionTitle'
+
 function TrendingComponent(){
     return<>
         <div className={style.trendingContainer}>
@@ -10,9 +11,9 @@ function TrendingComponent(){
                         <img className={style.featuredImage} src='/images/unique house design.jpg' alt='unique house design' />
                     </div>
                     <div className={style.articleContent}>
-                        <h5 className={style.articleTitle}>
+                        <h6 className={style.articleTitle}>
                             How to build your dream home in Kenya
-                        </h5>
+                        </h6>
                     </div>
                 </div>
                 <div className={style.article}>
@@ -20,9 +21,9 @@ function TrendingComponent(){
                         <img className={style.featuredImage} src='/images/Glass-Graham-House-by-E.-Cobb-Architects-900x751.jpg' alt='Glass-Graham-House-by-E.-Cobb-Architects-900x751' />
                     </div>
                     <div className={style.articleContent}>
-                        <h5 className={style.articleTitle}>
+                        <h6 className={style.articleTitle}>
                             Find an architect to build your dream house
-                        </h5>
+                        </h6>
                     </div>
                 </div>
                 <div className={style.article}>
@@ -30,9 +31,9 @@ function TrendingComponent(){
                         <img className={style.featuredImage} src='/images/unique house design.jpg' alt='unique house design' />
                     </div>
                     <div className={style.articleContent}>
-                        <h5 className={style.articleTitle}>
+                        <h6 className={style.articleTitle}>
                             How to build your dream home in Kenya
-                        </h5>
+                        </h6>
                     </div>
                 </div>
                 <div className={style.article}>
