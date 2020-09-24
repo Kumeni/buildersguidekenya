3d7a57db30ca66841349e61402151cc827cdaab9
Samson Kumeni <samsonkumeni8@gmail.com>
2020-09-24 15:20:54 +0300

    missing directory restored

---
+++ components/Article/articleTitle/articleTitle.js
@@ -0,0 +1,9 @@
+import style from './articleTitle.module.css'
+
+export default function ArticleTitle() {
+    return (
+        <div className={style.articleTitle + ' pt-2 pb-2'}>
+            <h3>SAY HELLO TO THE NEW BUILDERS GUIDE KENYA</h3>
+        </div>
+    )
+}
